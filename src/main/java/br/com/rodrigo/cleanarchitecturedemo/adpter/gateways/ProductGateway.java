ed1bf267602f4a6b6f5dcd7fe86a81463922e443
Rodrigo <rodrigopereira.dev@gmail.com>
2022-01-05 09:15:55 -0300

    Creating product gateway to run the apllication

--- src/main/java/br/com/rodrigo/cleanarchitecturedemo/adpter/gateways/ProductGateway.java
+++ src/main/java/br/com/rodrigo/cleanarchitecturedemo/adpter/gateways/ProductGateway.java
@@ -1,2 +1,33 @@
-package br.com.rodrigo.cleanarchitecturedemo.adpter.gateways;public class ProductGateway {
+package br.com.rodrigo.cleanarchitecturedemo.adpter.gateways;
+
+import br.com.rodrigo.cleanarchitecturedemo.domain.models.Product;
+import br.com.rodrigo.cleanarchitecturedemo.domain.userCases.Ports.ProductPort;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class ProductGateway implements ProductPort {
+    @Override
+    public void save(Product product) {
+
+    }
+
+    @Override
+    public Optional<Product> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Product> findAll() {
+        return null;
+    }
+
+    @Override
+    public boolean productExists(Long id) {
+        return false;
+    }
 }
